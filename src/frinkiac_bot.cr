4fb535da670eca06fb136d4391abfe806f29cd85
Leah <leah.eramo@gmail.com>
2019-07-17 16:18:53 +1000

    ploise work

--- src/frinkiac_bot.cr
+++ src/frinkiac_bot.cr
@@ -4,22 +4,21 @@ require "./commands/*"
 module FrinkiacBot
   app = App.singleton
   bot = Bot.new(ENV["BOT_TOKEN"])
-  
-  app.add_command(
-    SimpsonsMemeCommand.new(
-      bot,
-      CommandType::DEMAND,
-      "/simpsons meme me (.+)/"
-    )
+
+
+  meme_command = SimpsonsMemeCommand.new(
+    bot,
+    CommandType::HEAR,
+    "/simpsons meme me (.+)/"
+  )
+  image_command = SimpsonsImageCommand.new(
+    bot,
+    CommandType::HEAR,
+    "/simpsons image me (.+)/"
   )
 
-  app.add_command(
-    SimpsonsImageCommand.new(
-      bot,
-      CommandType::DEMAND,
-      "/simpsons image me (.+)/"
-    )
-  )
+  app.add_command(meme_command)
+  app.add_command(image_command)
 
   app.add_bot(bot)
   app.run_app_server
